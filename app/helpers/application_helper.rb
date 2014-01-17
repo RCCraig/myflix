e893e9d51b0336a81ed0964a0e448546f4375dc6
Craig Chambers <craig__chambers@hotmail.com>
2014-01-18 00:15:23 +0900

    mod5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
-module ApplicationHelper 
+module ApplicationHelper
+
+def options_for_video_reviews(selected=nil)
+  options_for_select((1..5).map {|number| [pluralize(number, "Star"), number]}, selected)
+end 
 end
