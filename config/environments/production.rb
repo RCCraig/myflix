837d282bccdc1e9bc4f83defcd300e6d3d6e9e59
Craig Chambers <craig__chambers@hotmail.com>
2014-02-19 19:56:06 +0900

    mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,17 +16,16 @@ Myflix::Application.configure do
 
   config.i18n.fallbacks = true
 
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :notify    
 
-  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.delivery_method = :smtp
 
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
-    :authentication => :plain,
-  }
-  ActionMailer::Base.delivery_method = :smtp
-end
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['gmail_username'],
+    password:             ENV['gmail_password'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  end
